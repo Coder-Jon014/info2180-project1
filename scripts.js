aa281decb40e103360835f0f935d0561a217a8e3
Coder-Jon014 <jon.coombs14@gmail.com>
2021-10-21 23:45:56 -0500

    Edited Webpage Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function(){
+ 
+    var email = document.getElementById("email");
+    var message = document.getElementsByClassName("message")[0];
+    var submit = document.getElementsByTagName("button")[0];
+    
+    submit.addEventListener("click", function(e){
+     e.preventDefault();
+     
+     if (email.value == ""){
+      message.innerHTML = "Email is required for submission"
+     }
+           else{
+      message.innerHTML = "Thank You For Subscribing! Your email has been submitted";
+     }
+    });
+   });
